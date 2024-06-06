1ece5d71cca9c52e7c477e71b85dc070091717c5
Hozmel Roberto De la Rosa Rendón <hozdelarosa@outlook.com>
2024-06-05 19:01:16 -0600

    Adding deleting the local storage in the button to see the result

--- src/components/areas/finished.tsx
+++ src/components/areas/finished.tsx
@@ -1,8 +1,18 @@
-export default function FinishedArea({ children }: { children: React.ReactNode}) {
+"use client"
+
+import Link from "next/link";
+
+function remo() {
+  window.localStorage.removeItem("repuestasUser");
+  window.localStorage.removeItem("preguntaActual");
+}
+
+export default function FinishedArea() {
   return (
     <main className="flex items-center h-[89vh]">
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+      </div>
 
       <div className="mx-5 sm:mx-auto sm:max-w-5xl">
         <div className="text-center">
@@ -20,7 +30,14 @@ export default function FinishedArea({ children }: { children: React.ReactNode})
             resultado de tu prueba presiona el boton:
           </p>
           <div className="mt-5 sm:mt-10 flex items-center justify-center gap-x-6">
-            {children}
+            <button onClick={remo}>
+              <Link
+                href="/test/result"
+                className="bg-blue-950 hover:bg-blue-900 text-white border-blue-950 border-2 font-medium transition-all ease-in-out duration-300 rounded-lg text-base px-5 py-2 md:px-8 md:py-2.5 cursor-pointer"
+                >
+                Resultado
+              </Link>
+            </button>
           </div>
         </div>
       </div>
