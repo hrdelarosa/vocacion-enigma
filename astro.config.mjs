69b697cc161b4ad8eb15d4762cdbd7849a82af0d
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-07-15 21:10:15 -0600

    Add global styles and update TypeScript configuration
    - Created a new global CSS file with Tailwind CSS imports, custom themes, and animations.
    - Defined font-face for 'Inter' and added various color variables for light and dark themes.
    - Implemented smooth scrolling and transition effects for background colors.
    - Updated tsconfig.json to include JSX support for Preact and adjusted include/exclude settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  env: {
+    schema: {
+      SUPABASE_URL: envField.string({ context: 'server', access: 'public' }),
+      SUPABASE_ANON_KEY: envField.string({
+        context: 'server',
+        access: 'public',
+      }),
+    },
+  },
+})
